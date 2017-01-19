1f3f3d908842138a1b3336108b89bff0a5d752a3
Tarciso-Filho <tarciso.filho@hotmail.com>
2017-01-19 16:27:23 -0300

    Primeiros passos
    classes importadas

---
+++ FolhaDePagamento.java
@@ -0,0 +1,13 @@
+package folhadepagamento;
+import java.util.Scanner;
+public class FolhaDePagamento
+{
+    public static void main(String[] args)
+    {
+        // MENU
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.println("FOLHA DE PAGAMENTO - Tarciso Filho");
+        
+    }//676523
+}
